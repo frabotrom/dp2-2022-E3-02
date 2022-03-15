a8b56644e755bab3452b15228faeddeaeeac231f
pabquide <48489345+pabquide@users.noreply.github.com>
2022-03-15 14:17:20 +0100

    Corrección de fallo

--- src/main/java/acme/entities/Tool.java
+++ src/main/java/acme/entities/Tool.java
@@ -4,9 +4,9 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -24,7 +24,7 @@ public class Tool extends AbstractEntity{
 	private static final long serialVersionUID = 1L;
 
 	@NotBlank
-	@Length(min=101)
+	@Length(max=101)
 	protected String name;
 	
 	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
@@ -32,14 +32,14 @@ public class Tool extends AbstractEntity{
 	protected String code;
 	
 	@NotBlank
-	@Length(min=101)
+	@Length(max=101)
 	protected String technology;
 	
 	@NotBlank
-	@Length(min=256)
+	@Length(max=256)
 	protected String description;
 	
-	@Size(min=1)
+	@Min(1)
 	protected Double retailPrice;
 	
 	@URL
