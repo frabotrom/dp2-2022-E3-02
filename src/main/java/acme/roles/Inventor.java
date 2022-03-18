19543fc27b728aa183baf55a74dcd6952239cc4c
fausto4us <fauvazrod@alum.us.es>
2022-03-18 21:49:19 +0100

    Hostfix: done

--- src/main/java/acme/roles/Inventor.java
+++ src/main/java/acme/roles/Inventor.java
@@ -13,36 +13,20 @@
 package acme.roles;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotBlank;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
-
-import acme.framework.roles.UserRole;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Inventor extends UserRole {
+public class Inventor extends Role {
 
 	// Serialisation identifier -----------------------------------------------
 
 	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
-
-	@NotBlank
-	@Length(max = 101)
-	protected String			company;
-
-	@NotBlank
-	@Length(max = 256)
-	protected String			statement;
-	
-	@URL
-	protected String			info;
 
 	// Derived attributes -----------------------------------------------------
 
